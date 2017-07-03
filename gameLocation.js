eb8577b1e46da3b8df40d2ade46d00910972c451
Nikita Chernyakov <nikchernyakov@yandex.ru>
2017-07-03 23:15:11 +0300

    Add generation buildings on location

--- gameLocation.js
+++ gameLocation.js
@@ -4,7 +4,10 @@ function createLocation() {
         fillColor: '#c5c5c5',
         w: 2000, h: 2000
     });
+
     location.setUserData({
+
+        borderSize: 50,
 
         spawns: [],
         loots: [],
@@ -19,33 +22,39 @@ function createLocation() {
             OOP.drawArr(this.loots);
             OOP.drawArr(this.buildings);
         },
+
         fillRandomLocation: function () {
+            // Create walls
             this.walls.push(game.newRectObject({
                 fillColor: '#000000',
                 position: point(0,0),
-                w: 2000, h: 50
+                w: this.w, h: this.borderSize
             }));
             this.walls.push(game.newRectObject({
                 fillColor: '#000000',
                 position: point(0,0),
-                w: 50, h: 2000
+                w: this.borderSize, h: this.h
             }));
             this.walls.push(game.newRectObject({
                 fillColor: '#000000',
                 position: point(0,2000-50),
-                w: 2000, h: 50
+                w: this.w, h: this.borderSize
             }));
             this.walls.push(game.newRectObject({
                 fillColor: '#000000',
                 position: point(2000-50,0),
-                w: 50, h: 2000
+                w: this.borderSize, h: this.h
             }));
+
+            var buildingsCount = math.random(3,5);
+            for(var i = 0; i < buildingsCount; i++){
+                this.buildings.push(createRandomBuilding(this));
+            }
 
             /*var spawnsCount = math.random(12, 16);
             OOP.forInt(spawnsCount, function () {
                 this.spawns.push(createRandomSpawn());
             });*/
-
 
         },
         redrawLocation: function () {
@@ -53,9 +62,53 @@ function createLocation() {
             this.drawAllObjects();
         },
 
-        getStartPlayerPosition: function(){
+        getPlayerStartPosition: function(){
             return point(2000 - 200, 2000 - 200);
+        },
+
+        getPlacesArray: function() {
+            return [this.spawns, this.buildings];
         }
+
     });
     return location;
+}
+
+function checkPositionForIntersect(location, pos) {
+    var newObj = game.newBaseObject({
+            position: pos
+        }),
+        flag = false;
+
+    OOP.forEach(location.getPlacesArray(), function (place) {
+        flag = newObj.isArrIntersect(place);
+    });
+    return flag;
+}
+
+/** Building functions */
+function createBuilding(pos, w, h) {
+    return game.newRectObject({
+        positionC: pos,
+        fillColor: "#07ff00",
+        alpha: 0.3,
+        w: w, h: h
+    });
+}
+
+function createRandomBuilding(location){
+    var buildingW = math.random(400,600),
+        buildingH = math.random(400,600),
+        buildingPosC = generateRandomPos(location, buildingW, buildingH);
+
+    while(checkPositionForIntersect(location, buildingPosC))
+        buildingPosC = generateRandomPos(location, buildingW, buildingH);
+    return createBuilding(buildingPosC, buildingW, buildingH);
+}
+
+function generateRandomPos(location, objW, objH) {
+    return point(math.random(location.borderSize + objW / 2,
+            location.w - location.borderSize - objW / 2),
+        math.random(location.borderSize + objH / 2,
+            location.h - location.borderSize - objH / 2));
 }
