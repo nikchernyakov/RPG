f6950ae4f4aaf5e39495f871541bc3b5a50773fa
Nikita Chernyakov <nikchernyakov@yandex.ru>
2017-09-14 18:26:26 +0300

    Add drawing FPS

--- game.js
+++ game.js
@@ -41,6 +41,15 @@ game.newLoopFromConstructor('start', function () {
         player.checkSkills();
         player.checkLoot();
 
+        /*brush.drawText({
+            x: mouse.getPosition().x, y: mouse.getPosition().y,
+            text : 'FPS: ' + game.getFPS(),
+            size : 30,
+            color : 'black',
+            strokeColor : 'white',
+            strokeWidth : 1,
+            style : 'bold'
+        });*/
 
     };
 
